53822c72132db7e1fff87eda85243ab90c7b7d28
Miguel Chen <vvchan@outlook.com>
2022-11-15 14:05:00 -0500

    init

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+
+//define your routes here. don't forget about error handling
+app.get('/', function (request, response) {
+  response.json({
+    message: 'Test',
+  });
+});
+
+app.all('*', (req, res) => {
+  res.status(404);
+  res.send('not found');
+});
+
+// global error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('server error, please try again later');
+});
+
+// listen for requests :)
+const listener = app.listen(parseInt(process.env.PORT) + 1, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
