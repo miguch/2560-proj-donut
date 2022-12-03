92c4ca1612c745740e403512527fcb2d1d804e88
Miguel Chen <vvchan@outlook.com>
2022-12-03 12:23:49 -0500

    test cors

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
+const cors = require('cors');
+
 app.use(express.json());
+app.use(cors());
 
 const mongoose = require("mongoose");
 
